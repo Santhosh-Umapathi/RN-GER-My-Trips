530c44efdc7ba031fa0cf40f4c302026f41a9bdd
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2021-10-06 17:57:39 +0200

    FEAT: File system connected

--- src/store/action.js
+++ src/store/action.js
@@ -1,8 +1,24 @@
 import { ADD_PLACE } from "./reducer";
+import * as FileSystem from "expo-file-system";
 
 export const addPlace = (title, image) => {
-  return {
-    type: ADD_PLACE,
-    payload: { title, image },
+  return async (dispatch) => {
+    const newFileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + newFileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+
+      dispatch({
+        type: ADD_PLACE,
+        payload: { title, image: newPath },
+      });
+    } catch (error) {
+      console.log("🚀 --- return --- error", error);
+      throw error;
+    }
   };
 };
