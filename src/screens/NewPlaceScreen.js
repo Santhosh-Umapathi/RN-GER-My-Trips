a64744de96d1af4eca7dfc1ee9ce5c5e58ac8fde
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2021-10-09 09:30:14 +0200

    FEAT: OTA check with expo publish(SUCCESS)

--- src/screens/NewPlaceScreen.js
+++ src/screens/NewPlaceScreen.js
@@ -50,7 +50,7 @@ const NewPlaceScreen = (props) => {
 };
 
 NewPlaceScreen.navigationOptions = {
-  headerTitle: "Add Place",
+  headerTitle: "Add New Place",
 };
 
 const styles = StyleSheet.create({
